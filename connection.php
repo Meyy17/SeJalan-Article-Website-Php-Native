484f90cee00db5d6ff971ef0a5925537d4a0693d
Meyy17 <qilaadikara@gmail.com>
2023-03-16 11:50:33 +0700

    first commit

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'sejalan_db';
+
+$conn = mysqli_connect($hostname, $username, $password, $dbname, '3307') or die("Invalid Db");
